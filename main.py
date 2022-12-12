32b41af447f33e65fec5da3b604dde1511987a91
Barbary-Theo <theo.barbary25@gmail.com>
2022-12-12 15:43:56 +0100

    among us

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import tensorboard
-import keras as keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras import preprocessing
-from numpy import array, zeros
+#import keras as keras
+#from keras.models import Sequential
+#from keras.layers import Dense
+#from keras import preprocessing
+#from numpy import array, zeros
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 
 
@@ -14,14 +13,28 @@ def read_csv_file(file_name):
     veneneux = []
     commestible = []
 
-
     with open(file_name, "r") as file:
-        for line in file.read():
-            print(line)
+
+        lines = file.read().split("\n")
+        index = 0
+
+        for line in lines:
+            if index != 0:
+                if line[0].__contains__("e"):
+                    commestible.append(line)
+                else:
+                    veneneux.append(line)
+            index += 1
+
+
+    return veneneux, commestible
 
 
 def main():
-    read_csv_file("mushrooms.csv")
+
+    result= read_csv_file("mushrooms.csv")
+
+    print(f"Veneer : {len(result[0])}, comestibles : {len(result[1])}")
 
 
 if __name__ == "__main__":
